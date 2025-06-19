14c2905ecc3710624f1e5afa45208faed82e3de3
Tanuja kavilkar <tanujakavilkar1357@gmail.com>
2025-06-19 10:57:31 +0530

    new code add

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify, render_template
 from flask_cors import CORS
 import joblib
 
 app=Flask(__name__)
 CORS(app)
 model=joblib.load('house_price_model.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -15,4 +19,5 @@ def predict():
     return jsonify({'predicted_price': prediction[0]})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5500)
+
